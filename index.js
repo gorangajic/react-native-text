c33675181fccf154a1dcc24b3f299113e6205e5a
Goran Gajic <fero.null@gmail.com>
2016-10-14 18:47:36 +0200

    check if style is provided

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ const flattenStyle = StyleSheet.flatten;
 const realWidth = height > width ? width : height;
 
 const ScalableText = ({ style, children, ...props }) => {
-  const fontSize = flattenStyle(style).fontSize || 14;
+  const flatStyle = flattenStyle(style);
+  const fontSize = flatStyle && flatStyle.fontSize ? flatStyle.fontSize : 14;
   const scaledFontSize = Math.round(fontSize * realWidth / 375);
 
   return (
